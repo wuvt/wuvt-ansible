9dd6c12f011af4f79283a893110347710ebf02ea
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-10-30 18:26:36 -0700

    remove deprecated and broken stuff

--- roles/ldap/templates/ldap-backup.sh
+++
@@ -1,7 +0,0 @@
-LDAPBK=ldap-directory-backup.ldif
-BACKUPDIR=/root
-REMOTEDIR=/mnt/mckillican/bak/hyattregency/
-/usr/sbin/slapcat -v -b "dc=wuvt,dc=vt,dc=edu" -l $BACKUPDIR/$LDAPBK
-gzip -9 $BACKUPDIR/$LDAPBK
-rsync -avHK $BACKUPDIR/$LDAPBK.gz mckillican:$REMOTEDIR
-
