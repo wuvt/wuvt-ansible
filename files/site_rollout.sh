e01ccab95964159d327947d77f03ed27f4ce2ee6
mutantmonkey <mutantmonkey@mutantmonkey.in>
2017-01-13 00:05:20 -0800

    drop wuvt-site-am from site_rollout.sh

--- files/site_rollout.sh
+++ files/site_rollout.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 for i in $(seq 1 2); do
-	fleetctl stop wuvt-site-{am,prod}-discovery@$i.service
+	fleetctl stop wuvt-site-prod-discovery@$i.service
 	sleep 2
-	fleetctl stop wuvt-site-{am,prod}@$i.service
+	fleetctl stop wuvt-site-prod@$i.service
 	sleep 2
-	fleetctl start wuvt-site-{am,prod}@$i.service
+	fleetctl start wuvt-site-prod@$i.service
 	sleep 2
-	fleetctl start wuvt-site-{am,prod}-discovery@$i.service
+	fleetctl start wuvt-site-prod-discovery@$i.service
 	sleep 10
 done
