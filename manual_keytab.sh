6e5095d710ead7abb34a08288cf74da059922631
mutantmonkey <mutantmonkey@mutantmonkey.in>
2017-01-17 22:55:11 -0800

    update manual keytab setup

--- manual_keytab.sh
+++ manual_keytab.sh
@@ -3,10 +3,11 @@
 # using the ipa tools. This is useful if you need to add a host to the domain
 # that is running an OS that does not ship the ipa tools.
 
-host=naro2.wuvt.vt.edu
+host=sasago.wuvt.vt.edu
+public_ipv4=192.168.0.157
 user=$(whoami)
 ipa_server=zolitude.wuvt.vt.edu
 
-ipa host-add $host
+ipa host-add $host --ip-address=$public_ipv4
 ipa host-allow-create-keytab $host --users=$user
 ipa-getkeytab -s $ipa_server -k $host.keytab -p host/$host
