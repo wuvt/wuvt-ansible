e8556abb960a3093715974905760cb865b9ede49
mutantmonkey <mutantmonkey@mutantmonkey.in>
2017-02-03 21:09:34 -0800

    cfssl_sign: Only sign certificates when necessary
    If there is an existing certificate, first check to see if it can still
    be used before signing a new one.

--- library/cfssl_sign.py
+++ library/cfssl_sign.py
@@ -2,13 +2,17 @@
 
 import base64
 import binascii
+import datetime
 import hashlib
 import hmac
 import json
+import os.path
 import requests
 import urlparse
 
 from ansible.module_utils.basic import AnsibleModule
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 
 
 DOCUMENTATION = '''
@@ -53,9 +57,31 @@ options:
       - The profile to provide to the cfssl endpoint.
     required: false
     default: null
+  force:
+    description:
+      - Sign the certificate even if there is a certificate at I(dest) that has already been signed and is not expiring soon.
+    required: false
+    default: false
+requirements:
+  - cryptography
+  - requests
 author:
     - "mutantmonkey"
 '''
+
+
+def should_sign(cert_path):
+    if not os.path.exists(cert_path):
+        return True
+
+    parsed_cert = x509.load_pem_x509_certificate(open(cert_path, 'rb').read(),
+                                                 default_backend())
+
+    next_sign = parsed_cert.not_valid_after - datetime.timedelta(days=15)
+    if datetime.datetime.utcnow() >= next_sign:
+        return True
+
+    return False
 
 
 def main():
@@ -68,8 +94,9 @@ def main():
             endpoint_ca=dict(default=None, type='path'),
             label=dict(default=None),
             profile=dict(default=None),
+            force=dict(default=False, type='bool'),
         ),
-        supports_check_mode=False
+        supports_check_mode=True
     )
 
     endpoint = module.params['endpoint']
@@ -77,47 +104,54 @@ def main():
     csr = module.params['csr']
     dest = module.params['dest']
     endpoint_ca = module.params['endpoint_ca']
+    force = module.params['force']
 
-    request = {
-        'certificate_request': open(csr).read(),
-    }
+    if force or should_sign(dest):
+        if module.check_mode:
+            module.exit_json(changed=True)
+        else:
+            request = {
+                'certificate_request': open(csr).read(),
+            }
 
-    for arg in ('label', 'profile'):
-        if module.params[arg] is not None:
-            request[arg] = module.params[arg]
+            for arg in ('label', 'profile'):
+                if module.params[arg] is not None:
+                    request[arg] = module.params[arg]
 
-    if endpoint_ca is not None:
-        verify = endpoint_ca
+            if endpoint_ca is not None:
+                verify = endpoint_ca
+            else:
+                verify = True
+
+            if auth_key is not None:
+                h = hmac.new(binascii.unhexlify(auth_key), json.dumps(request),
+                             hashlib.sha256)
+
+                r = requests.post(
+                    urlparse.urljoin(endpoint, 'api/v1/cfssl/authsign'),
+                    data=json.dumps({
+                        'token': base64.b64encode(h.digest()),
+                        'request': base64.b64encode(json.dumps(request)),
+                    }),
+                    verify=verify,
+                )
+            else:
+                r = requests.post(
+                    urlparse.urljoin(endpoint, 'api/v1/cfssl/sign'),
+                    data=json.dumps(request),
+                    verify=verify,
+                )
+
+            response = r.json()
+            if response['success'] is True:
+                with open(dest, 'w') as f:
+                    f.write(response['result']['certificate'].strip())
+                module.exit_json(changed=True)
+            else:
+                module.fail_json(msg="Failed to sign certificate.",
+                                 errors=response['errors'])
     else:
-        verify = True
-
-    if auth_key is not None:
-        h = hmac.new(binascii.unhexlify(auth_key), json.dumps(request),
-                     hashlib.sha256)
-
-        r = requests.post(
-            urlparse.urljoin(endpoint, 'api/v1/cfssl/authsign'),
-            data=json.dumps({
-                'token': base64.b64encode(h.digest()),
-                'request': base64.b64encode(json.dumps(request)),
-            }),
-            verify=verify,
-        )
-    else:
-        r = requests.post(
-            urlparse.urljoin(endpoint, 'api/v1/cfssl/sign'),
-            data=json.dumps(request),
-            verify=verify,
-        )
-
-    response = r.json()
-    if response['success'] is True:
-        with open(dest, 'w') as f:
-            f.write(response['result']['certificate'].strip())
-        module.exit_json(changed=True)
-    else:
-        module.fail_json(msg="Failed to sign certificate.",
-                         errors=response['errors'])
+        module.exit_json(changed=False)
 
 
 if __name__ == '__main__':
