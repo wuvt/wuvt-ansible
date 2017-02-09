d5376862f8e4015e7604da3797bf91d16f41eee3
mutantmonkey <mutantmonkey@mutantmonkey.in>
2017-02-08 21:41:58 -0800

    Add gen_icecast_cert.sh to stream role
    We don't have a task to install this yet, but I don't want to lose this
    script.

---
+++ roles/stream/files/gen_icecast_cert.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+basedir="/etc/letsencrypt/live/stream.wuvt.vt.edu"
+
+install -o icecast -g icecast -m 600 $basedir/fullchain.pem /etc/icecast_tls.pem
+cat $basedir/privkey.pem >> /etc/icecast_tls.pem
+service icecast restart
