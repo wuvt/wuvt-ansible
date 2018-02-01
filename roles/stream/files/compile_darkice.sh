ec3cbd00c71c505f6cb72d6fe43d4cbc41f9bfc4
mutantmonkey <mutantmonkey@mutantmonkey.in>
2018-01-31 20:12:50 -0800

    stream: Remove darkice stuff
    We are no longer using darkice, so let's remove it.

--- roles/stream/files/compile_darkice.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /usr/local/src
-svn co http://darkice.googlecode.com/svn/darkice/trunk darkice-read-only
-cd darkice-read-only
-./autogen.sh
-./configure --with-faac-prefix=/usr/local
-make
-make install
