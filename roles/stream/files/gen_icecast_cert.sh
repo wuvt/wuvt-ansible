f2b33dc22d59ffb3ddb8f4cafc79ca39889262cd
mutantmonkey <mutantmonkey@mutantmonkey.in>
2018-01-31 20:32:38 -0800

    stream: update icecast config, remove old stuff

--- roles/stream/files/gen_icecast_cert.sh
+++ roles/stream/files/gen_icecast_cert.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 basedir="/etc/letsencrypt/live/stream.wuvt.vt.edu"
 
-install -o icecast -g icecast -m 600 $basedir/fullchain.pem /etc/icecast_tls.pem
-cat $basedir/privkey.pem >> /etc/icecast_tls.pem
+install -o icecast2 -g icecast -m 600 $basedir/fullchain.pem /etc/icecast2/icecast_tls.pem
+cat $basedir/privkey.pem >> /etc/icecast2/icecast_tls.pem
 
 service icecast restart
-service darkice restart
