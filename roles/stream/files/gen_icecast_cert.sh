f668fd0068edd1b19a3a8e55614e494ef96feb42
mutantmonkey <mutantmonkey@mutantmonkey.in>
2018-04-09 22:49:42 -0700

    stream: update gen_icecast_cert.sh

--- roles/stream/files/gen_icecast_cert.sh
+++ roles/stream/files/gen_icecast_cert.sh
@@ -4,4 +4,4 @@ basedir="/etc/letsencrypt/live/stream.wuvt.vt.edu"
 install -o icecast2 -g icecast -m 600 $basedir/fullchain.pem /etc/icecast2/icecast_tls.pem
 cat $basedir/privkey.pem >> /etc/icecast2/icecast_tls.pem
 
-service icecast restart
+systemctl restart icecast2.service
